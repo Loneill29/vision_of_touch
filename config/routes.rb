36ac908e73dfad88ca2fb88f91667ae81925c2b2
Linda <oneill.linda21@gmail.com>
2018-07-11 11:18:48 -0500

    add calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'calendars/index'
+
+  get 'calendars/show'
+
+  get 'calendars/new'
+
   devise_for :users
   get 'welcome/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607234412) do
+ActiveRecord::Schema.define(version: 20180711144525) do
+
+  create_table "calendars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
